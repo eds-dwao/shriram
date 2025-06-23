b6f7519c88949fbc44d0712da0614df04846eea8
tahirtcz <tahirtkd7@gmail.com>
2025-06-23 19:17:41 +0530

    faq block

---
+++ blocks/shriram6/shriram6.js
@@ -0,0 +1,340 @@
+export default function decorate(block) {
+  const container = document.createElement('div');
+  container.setAttribute('class', 'container');
+
+  const sectionEl = document.createElement('section');
+  sectionEl.setAttribute('_ngcontent-serverapp-c4208776966', '');
+  sectionEl.setAttribute('class', 'spacer-pt-space spacer-pb-space');
+
+    const divEl = document.createElement('div');
+    divEl.setAttribute('_ngcontent-serverapp-c4208776966', '');
+    divEl.setAttribute('class', 'container');
+
+      const divEl2 = document.createElement('div');
+      divEl2.setAttribute('_ngcontent-serverapp-c4208776966', '');
+      divEl2.setAttribute('class', 'sitemap');
+
+        const h1El = document.createElement('h1');
+        h1El.setAttribute('_ngcontent-serverapp-c4208776966', '');
+        h1El.setAttribute('class', 'common__heading');
+        h1El.textContent = 'Sitemap';
+      divEl2.append(h1El);
+
+        const h2El = document.createElement('h2');
+        h2El.setAttribute('_ngcontent-serverapp-c4208776966', '');
+        h2El.setAttribute('class', 'sitemap__heading');
+        h2El.textContent = 'HTML Sitemap of Shriram Finance Limited';
+      divEl2.append(h2El);
+
+        const ulEl = document.createElement('ul');
+        ulEl.setAttribute('_ngcontent-serverapp-c4208776966', '');
+        ulEl.setAttribute('class', 'sitemap__content');
+
+          const liEl = document.createElement('li');
+          liEl.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl = document.createElement('a');
+            aEl.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl.setAttribute('href', '/sitemap/products');
+            aEl.textContent = 'Products';
+          liEl.append(aEl);
+        ulEl.append(liEl);
+
+          const liEl2 = document.createElement('li');
+          liEl2.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl2 = document.createElement('a');
+            aEl2.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl2.setAttribute('href', '/sitemap/resources');
+            aEl2.textContent = 'Resources';
+          liEl2.append(aEl2);
+        ulEl.append(liEl2);
+
+          const liEl3 = document.createElement('li');
+          liEl3.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl3 = document.createElement('a');
+            aEl3.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl3.setAttribute('href', '/sitemap/investors');
+            aEl3.textContent = 'Investors';
+          liEl3.append(aEl3);
+        ulEl.append(liEl3);
+
+          const liEl4 = document.createElement('li');
+          liEl4.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl4 = document.createElement('a');
+            aEl4.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl4.setAttribute('href', '/sitemap/calculator');
+            aEl4.textContent = 'Calculator';
+          liEl4.append(aEl4);
+        ulEl.append(liEl4);
+
+          const liEl5 = document.createElement('li');
+          liEl5.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl5 = document.createElement('a');
+            aEl5.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl5.setAttribute('href', '/sitemap/discover-shriram-finance');
+            aEl5.textContent = 'Discover Shriram Finance';
+          liEl5.append(aEl5);
+        ulEl.append(liEl5);
+
+          const liEl6 = document.createElement('li');
+          liEl6.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl6 = document.createElement('a');
+            aEl6.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl6.setAttribute('href', '/sitemap/faqs');
+            aEl6.textContent = 'FAQs';
+          liEl6.append(aEl6);
+        ulEl.append(liEl6);
+
+          const liEl7 = document.createElement('li');
+          liEl7.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl7 = document.createElement('a');
+            aEl7.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl7.setAttribute('href', '/sitemap/footer');
+            aEl7.textContent = 'Footer';
+          liEl7.append(aEl7);
+        ulEl.append(liEl7);
+
+          const liEl8 = document.createElement('li');
+          liEl8.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl8 = document.createElement('a');
+            aEl8.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl8.setAttribute('href', '/sitemap/contact-us');
+            aEl8.textContent = 'Contact Us';
+          liEl8.append(aEl8);
+        ulEl.append(liEl8);
+
+          const liEl9 = document.createElement('li');
+          liEl9.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl9 = document.createElement('a');
+            aEl9.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl9.setAttribute('href', '/sitemap/pay-online');
+            aEl9.textContent = 'Pay Online';
+          liEl9.append(aEl9);
+        ulEl.append(liEl9);
+
+          const liEl10 = document.createElement('li');
+          liEl10.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl10 = document.createElement('a');
+            aEl10.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl10.setAttribute('href', '/sitemap/shriram-one');
+            aEl10.textContent = 'Shriram One';
+          liEl10.append(aEl10);
+        ulEl.append(liEl10);
+
+          const liEl11 = document.createElement('li');
+          liEl11.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl11 = document.createElement('a');
+            aEl11.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl11.setAttribute('href', '/sitemap/credit-score');
+            aEl11.textContent = 'Credit Score';
+          liEl11.append(aEl11);
+        ulEl.append(liEl11);
+
+          const liEl12 = document.createElement('li');
+          liEl12.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl12 = document.createElement('a');
+            aEl12.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl12.setAttribute('href', '/sitemap/insurance');
+            aEl12.textContent = 'Insurance';
+          liEl12.append(aEl12);
+        ulEl.append(liEl12);
+
+          const liEl13 = document.createElement('li');
+          liEl13.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl13 = document.createElement('a');
+            aEl13.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl13.setAttribute('href', '/sitemap/rewards');
+            aEl13.textContent = 'Rewards';
+          liEl13.append(aEl13);
+        ulEl.append(liEl13);
+
+          const liEl14 = document.createElement('li');
+          liEl14.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl14 = document.createElement('a');
+            aEl14.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl14.setAttribute('href', '/sitemap/bharat-bill-payment-system');
+            aEl14.textContent = 'Bharat Bill Payment System';
+          liEl14.append(aEl14);
+        ulEl.append(liEl14);
+
+          const liEl15 = document.createElement('li');
+          liEl15.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl15 = document.createElement('a');
+            aEl15.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl15.setAttribute('href', '/sitemap/financial-faq');
+            aEl15.textContent = 'Financial FAQ';
+          liEl15.append(aEl15);
+        ulEl.append(liEl15);
+
+          const liEl16 = document.createElement('li');
+          liEl16.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl16 = document.createElement('a');
+            aEl16.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl16.setAttribute('href', '/sitemap/cylinder-gas-booking');
+            aEl16.textContent = 'Cylinder Gas Booking';
+          liEl16.append(aEl16);
+        ulEl.append(liEl16);
+
+          const liEl17 = document.createElement('li');
+          liEl17.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl17 = document.createElement('a');
+            aEl17.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl17.setAttribute('href', '/sitemap/mobile-postpaid-bill-payment');
+            aEl17.textContent = 'Mobile Postpaid Bill Payment';
+          liEl17.append(aEl17);
+        ulEl.append(liEl17);
+
+          const liEl18 = document.createElement('li');
+          liEl18.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl18 = document.createElement('a');
+            aEl18.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl18.setAttribute('href', '/sitemap/broadband-bill-payment');
+            aEl18.textContent = 'Broadband Bill Payment';
+          liEl18.append(aEl18);
+        ulEl.append(liEl18);
+
+          const liEl19 = document.createElement('li');
+          liEl19.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl19 = document.createElement('a');
+            aEl19.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl19.setAttribute('href', '/sitemap/dth-recharge');
+            aEl19.textContent = 'Dth Recharge';
+          liEl19.append(aEl19);
+        ulEl.append(liEl19);
+
+          const liEl20 = document.createElement('li');
+          liEl20.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl20 = document.createElement('a');
+            aEl20.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl20.setAttribute('href', '/sitemap/hospital-bill-payment');
+            aEl20.textContent = 'Hospital Bill Payment';
+          liEl20.append(aEl20);
+        ulEl.append(liEl20);
+
+          const liEl21 = document.createElement('li');
+          liEl21.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl21 = document.createElement('a');
+            aEl21.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl21.setAttribute('href', '/sitemap/cable-tv-recharge');
+            aEl21.textContent = 'Cable Tv Recharge';
+          liEl21.append(aEl21);
+        ulEl.append(liEl21);
+
+          const liEl22 = document.createElement('li');
+          liEl22.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl22 = document.createElement('a');
+            aEl22.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl22.setAttribute('href', '/sitemap/credit-card-bill-payment');
+            aEl22.textContent = 'Credit Card Bill Payment';
+          liEl22.append(aEl22);
+        ulEl.append(liEl22);
+
+          const liEl23 = document.createElement('li');
+          liEl23.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl23 = document.createElement('a');
+            aEl23.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl23.setAttribute('href', '/sitemap/municipal-services-and-taxes');
+            aEl23.textContent = 'Municipal Services And Taxes';
+          liEl23.append(aEl23);
+        ulEl.append(liEl23);
+
+          const liEl24 = document.createElement('li');
+          liEl24.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl24 = document.createElement('a');
+            aEl24.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl24.setAttribute('href', '/sitemap/landline-bill-payment');
+            aEl24.textContent = 'Landline Bill Payment';
+          liEl24.append(aEl24);
+        ulEl.append(liEl24);
+
+          const liEl25 = document.createElement('li');
+          liEl25.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl25 = document.createElement('a');
+            aEl25.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl25.setAttribute('href', '/sitemap/loan-repayment');
+            aEl25.textContent = 'Loan Repayment';
+          liEl25.append(aEl25);
+        ulEl.append(liEl25);
+
+          const liEl26 = document.createElement('li');
+          liEl26.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl26 = document.createElement('a');
+            aEl26.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl26.setAttribute('href', '/sitemap/electricity-bill-payment');
+            aEl26.textContent = 'Electricity Bill Payment';
+          liEl26.append(aEl26);
+        ulEl.append(liEl26);
+
+          const liEl27 = document.createElement('li');
+          liEl27.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl27 = document.createElement('a');
+            aEl27.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl27.setAttribute('href', '/sitemap/water-bill-payment');
+            aEl27.textContent = 'Water Bill Payment';
+          liEl27.append(aEl27);
+        ulEl.append(liEl27);
+
+          const liEl28 = document.createElement('li');
+          liEl28.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl28 = document.createElement('a');
+            aEl28.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl28.setAttribute('href', '/sitemap/fastag-recharge');
+            aEl28.textContent = 'Fastag Recharge';
+          liEl28.append(aEl28);
+        ulEl.append(liEl28);
+
+          const liEl29 = document.createElement('li');
+          liEl29.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl29 = document.createElement('a');
+            aEl29.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl29.setAttribute('href', '/sitemap/gas-bill-payment');
+            aEl29.textContent = 'Gas Bill Payment';
+          liEl29.append(aEl29);
+        ulEl.append(liEl29);
+
+          const liEl30 = document.createElement('li');
+          liEl30.setAttribute('_ngcontent-serverapp-c4208776966', '');
+
+            const aEl30 = document.createElement('a');
+            aEl30.setAttribute('_ngcontent-serverapp-c4208776966', '');
+            aEl30.setAttribute('href', '/sitemap/fd-schemes');
+            aEl30.textContent = 'Fd Schemes';
+          liEl30.append(aEl30);
+        ulEl.append(liEl30);
+      divEl2.append(ulEl);
+    divEl.append(divEl2);
+  sectionEl.append(divEl);
+  container.append(sectionEl);
+
+
+  block.textContent = '';
+  block.append(container);
+}
